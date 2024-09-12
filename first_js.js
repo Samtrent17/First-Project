903b83f19d06fe4745a08b40bf04cb1323f97821
Samtrent17 <xamsamuel079@gmail.com>
2024-09-12 19:19:35 +0300

    first java script and variables

---
+++ first_js.js
@@ -0,0 +1,8 @@
+// this is my first JS application
+console.log('Hello World')
+//
+console.log('B28338')
+console.log('Nangoli Samuel')
+console.log('M24B13/046')
+console.log('Bachelors Degree in Information Technology')
+
---
+++ variables.js
@@ -0,0 +1,25 @@
+
+// Innitialization
+var my_name = 'Nangoli Samuel'
+
+// declare a variable
+var height;
+
+// assign a value to the variable
+height = 5.4;
+
+//let 
+let area = 68.9
+
+console.log('area: ' + area)
+let fname = 'Nangoli'
+console.log(fname);
+console.log(fname," - ", height);
+
+//const
+//Innitialize
+const pie = 3.14;
+console.log(pie)
+
+
+
