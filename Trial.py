f11d7d8dc8865cef130616b6e14d82e8b2b32199
Samtrent17 <xamsamuel079@gmail.com>
2024-09-21 19:26:03 +0300

    Add files via upload

---
+++ Trial.py
@@ -0,0 +1,14 @@
+print('This is a backslash')
+
+name ='Sam'
+greeting = f'Wagwan, {name}'
+print(greeting)
+
+
+name = "Alice"
+greeting = f"Hello, {name}!"
+print(greeting)  
+
+name1 = 'Sam'
+name2 = 'Trent'
+print(name1 +" "+ name2)
